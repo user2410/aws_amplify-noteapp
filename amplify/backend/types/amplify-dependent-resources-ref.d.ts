9f88fb25584e71a53cc036c8eb3fd35ac40cf0cb
user2410 <dc241024@gmail.com>
2024-03-30 23:24:31 +0700

    ft: CRUD+L note

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifynoteapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "awsamplifynoteapp1946f423": {
       "AppClientID": "string",
